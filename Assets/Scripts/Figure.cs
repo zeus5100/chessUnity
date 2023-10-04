a4449bcad0f793a438070c8896507554850694e6
Krzysztof Lawniczak <krzysztof.lawniczak.1e@gmail.com>
2023-10-04 17:25:06 +0200

    dodalemJebacPis

--- Assets/Scripts/Figure.cs
+++ Assets/Scripts/Figure.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+
+//***** ***
 public class Figure : MonoBehaviour
 {
     [SerializeField] public string nameFigure;
